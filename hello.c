b5dca6560360fafa7334857420785783f082ae9c
AGUIZAV15 <jaguilarzavaleta1@gmail.com>
2022-10-07 14:31:11 -0500

    cambios nuevos

--- hello.c
+++ hello.c
@@ -3,7 +3,12 @@
 
 int main(){
     printf("Hola Mundo");
+    decirHola("Arturo");
     printf("Lo he modificado desde internet");
     return 0;
     //buen dia señores
+}
+
+void decirHola(char* nombre){
+    printf("Hola %s, ¿Como estas?", nombre);
 }
